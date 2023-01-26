45e872b914442412cdab4d41effb80a73922e27e
hassamahmedsiddiqui <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-01-26 20:14:35 +0500

    assigment 1 and using swal via cdn

---
+++ java/app.js
@@ -0,0 +1,32 @@
+// question no 1 or 2:
+// alert("Error! please enter a valid password")
+
+
+// question no 3:
+// alert("Welcome to Jscode\nHappy coding")
+
+
+// question no: 4
+// var name = "Welcome to js land"
+// alert(name)
+// var secondname= "Happy Coding"
+// alert(secondname)
+
+
+
+// // question no : 5
+// alert("hello.. i can run js through my web browser")
+
+
+// using swal via cdn
+// var name ="Using sweetalert , for practicing"?
+// swal("Good job!", "You clicked the button!", "success");
+
+
+
+swal({
+    title: "Practicing swal!",
+    text: "HASSAM AHMED!",
+    icon: "success",
+    button: "Aww yiss!",
+  });
